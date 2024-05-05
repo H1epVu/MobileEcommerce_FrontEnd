79214ec3d3e345aeb6dc72d99c3b3ce01feff4be
H1epVu <hiepvu.tech02@gmail.com>
2024-05-05 22:54:12 +0700

    Add search Item & Fix List Item

--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -9,7 +9,23 @@ import { FormatNumber } from '../../Utils';
 import Footer from "../../components/Footer";
 
 const Home = () => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [searchItem, setSearchItem] = useState('');
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        try {
+            const { data: products } = await axios.get(process.env.REACT_APP_PRODUCT_API + `${searchItem}`);
+            setProducts(products);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+        }
+    };
+
+    const handleInputChange = (e) => {
+        setSearchItem(e.target.value);
+    };
+
     useEffect(() => {
         const fetchData = async () => {
             const { data: products } = await axios.get(process.env.REACT_APP_PRODUCT_API)
@@ -17,24 +33,33 @@ const Home = () => {
         }
         fetchData()
     }, [])
+    
     return (
         <>
             <NavScroll />
-            <div className="home">
+            <div className="home m-3">
                 <section class="py-5 text-center container">
                     <div class="row py-lg-5 bg-light">
                         <div class="col-lg-6 col-md-8 mx-auto">
                             <h1 class="fw-light">IPHONE 15</h1>
                             <p class="lead text-body-secondary"> <b>A16 Bionic</b> tăng cường sức mạnh cho các tính năng tiên tiến. <b>Cùng thiết kế mới đầy sáng tạo</b> sử dụng mặt lưng kính được pha màu xuyên suốt toàn bộ chất liệu.</p>
                             <p>
-                                <Link class="btn btn-secondary my-2" to='/'>COMING SOON</Link>
+                                <Link class="btn btn-secondary my-2" to='/'>Coming Soon</Link>
                             </p>
                         </div>
                     </div>
                 </section>
-                <div className="product-list row">
+                <div className="search-bar m-3">
+                    <form className="form-inline my-2 my-lg-0" onSubmit={handleSearch}>
+                        <div className="d-flex">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Nhập tên sản phẩm" aria-label="Search" value={searchItem} onChange={handleInputChange} />
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="bi bi-search"></i></button>
+                        </div>
+                    </form>
+                </div>
+                <ul className="product-list mt-5">
                     {products.map((product) => (
-                        <div key={product._id} className="col-md-3">
+                        <li key={product._id} className="product-item">
                             <Card className="mb-3">
                                 <Card.Img className="d-flex justify-content-center" variant="top" src={product.imageUrl} />
                                 <Card.Body className="d-flex flex-column">
@@ -58,10 +83,9 @@ const Home = () => {
                                     </div>
                                 </Card.Body>
                             </Card>
-                        </div>
+                        </li>
                     ))}
-                </div>
-
+                </ul>
             </div>
             <Footer />
         </>
