24ebd0eb8b5affb23a90e21efbbf9f25922e0380
H1epVu <hiepvu.tech02@gmail.com>
2024-05-15 20:01:53 +0700

    change display + add search item

--- src/pages/admin/Mobile.jsx
+++ src/pages/admin/Mobile.jsx
@@ -7,6 +7,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Mobile = () => {
   const [mobiles, setMobiles] = useState([])
+  const [searchItem, setSearchItem] = useState('');
+
   const handleDelete = async (itemId) => {
     try {
       await axios.delete(process.env.REACT_APP_PRODUCT_API + `delete/${itemId}`, {
@@ -22,6 +24,20 @@ const Mobile = () => {
     }
   }
 
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const { data: products } = await axios.get(process.env.REACT_APP_PRODUCT_API + `${searchItem}`);
+      setMobiles(products);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setSearchItem(e.target.value);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get(process.env.REACT_APP_PRODUCT_API)
@@ -32,24 +48,34 @@ const Mobile = () => {
   return (
     <div className='container'>
       <h2 className='mt-5 mb-5'>Sản Phẩm</h2>
+      <div className="search-bar mb-4">
+        <form className="form-inline my-2 my-lg-0" onSubmit={handleSearch}>
+          <div className="d-flex">
+            <input className="form-control mr-sm-2" type="search" placeholder="Nhập tên sản phẩm" aria-label="Search" value={searchItem} onChange={handleInputChange} />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="bi bi-search"></i></button>
+          </div>
+        </form>
+      </div>
       <Table striped bordered hover>
         <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Status</th>
-            <th className='d-flex justify-content-center'><Link className='btn btn-success' to={`/admin/mobile/add`}>Thêm mới</Link></th>
+          <tr className='text-center align-middle'>
+            <th>IMG</th>
+            <th>Tên</th>
+            <th>Giá</th>
+            <th>Số lượng</th>
+            <th>Trạng thái</th>
+            <th className='d-flex justify-content-center'><Link className='btn btn-outline-success' to={`/admin/mobile/add`}>Thêm sản phẩm mới</Link></th>
           </tr>
         </thead>
         <tbody>
           {mobiles.map((item) => (
-            <tr key={item._id}>
-              <td>{item._id}</td>
+            <tr key={item._id} className="align-middle">
+              <td><img class="rounded mx-auto d-block" src={item.imageUrl} alt="" style={{ width: '80px', height: '80px' }} /></td>
               <td>{item.name}</td>
-              <td>{item.price}</td>
-              <td>{item.status === '0' ? 'Hết hàng' : 'Còn hàng'}</td>
-              <td style={{ textAlign: 'center' }}>
+              <td className='text-center'>{item.price} đ</td>
+              <td className='text-center'>{item.quantity}</td>
+              <td className='text-center'>{item.status === '0' ? 'Ngưng kinh doanh' : 'Đang mở'}</td>
+              <td className='text-center'>
                 <Link className='btn btn-primary' style={{ marginRight: '5px' }} to={`/admin/mobile/update/${item._id}`}>Edit</Link>
                 <Button variant="danger" onClick={() => handleDelete(item._id)}>Delete</Button>
               </td>
