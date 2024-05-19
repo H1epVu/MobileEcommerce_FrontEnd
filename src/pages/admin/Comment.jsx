1391dcfb6d36445b3a58a987c456f3eb9bd7ba9e
H1epVu <hiepvu.tech02@gmail.com>
2024-05-20 05:15:43 +0700

    change display

--- src/pages/admin/Comment.jsx
+++ src/pages/admin/Comment.jsx
@@ -1,57 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Table } from 'react-bootstrap';
-import axios from 'axios';
 import { Link } from "react-router-dom"
-import { toast } from 'react-toastify';
+import { Table } from 'react-bootstrap';
+import axios from 'axios';
 import "react-toastify/dist/ReactToastify.css";
-import { FormatDate } from '../../Utils';
 
 const Comment = () => {
-    const [comments, setComments] = useState([])
-    const handleDelete = async (cmtId) => {
-        try {
-            await axios.delete(process.env.REACT_APP_COMMENT_API + `delete/${cmtId}`, {
-                headers: {
-                    Authorization: 'Bearer ' + localStorage.getItem('token')
-                }
-            })
-            const updatedComments = comments.filter(cmt => cmt._id !== cmtId);
-            setComments(updatedComments);
-            toast.success('Xóa bình luận thành công!')
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    const [products, setProducts] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
-            const { data } = await axios.get(process.env.REACT_APP_COMMENT_API)
-            setComments(data)
+            const { data: prodData } = await axios.get(process.env.REACT_APP_PRODUCT_API)
+            setProducts(prodData)
         }
         fetchData()
     }, [])
+
     return (
         <div className='container'>
             <h2 className='mt-5 mb-5'>Bình luận</h2>
             <Table striped bordered hover>
                 <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Người dùng</th>
-                        <th>Nội dung</th>
-                        <th>Thời gian</th>
+                    <tr className='text-center align-middle'>
+                        <th>IMG</th>
+                        <th>Tên sản phẩm</th>
                         <th></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {comments.map((comment) => (
-                        <tr key={comment._id}>
-                            <td>{comment._id}</td>
-                            <td>{comment.email}</td>
-                            <td>{comment.content}</td>
-                            <td>{FormatDate(comment.createdAt)}</td>
+                    {products.map((prod) => (
+                        <tr key={prod._id} className="text-center align-middle">
+                            <td><img class="rounded mx-auto d-block" src={prod.imageUrl} alt="" style={{ width: '80px', height: '80px' }} /></td>
+                            <td>{prod.name}</td>
                             <td style={{ textAlign: 'center' }}>
-                                <Button className="btn btn-danger" onClick={() => handleDelete(comment._id)}>Delete</Button>
+                                <div>
+                                    <Link className='btn btn-primary' to={`/admin/comment/${prod._id}`}>Chi tiết</Link>
+                                </div>
                             </td>
                         </tr>
                     ))}
